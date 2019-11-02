ab41d08d21e3ed5bb5fae4e151b39b04e9b692ef
heidizhou <zzho938@aucklanduni.ac.nz>
2019-11-02 15:53:16 +1300

    Check if username exist

--- public/js/client-side.js
+++ public/js/client-side.js
@@ -8,6 +8,7 @@ document.getElementById("txtUsername").addEventListener("input", async function(
     let json = await response.json();
 
     let usernameInput = document.querySelector("#txtUsername").value;
+    
     console.log(usernameInput);
 for (let counter=0; counter < json.length; counter++) {
         if (JSON.stringify(json[counter].username) == `"${usernameInput}"`) {
