1fe41678d270cee179d179aad8baa0908a88f688
Gregory Rabone <grab079@UoA.auckland.ac.nz>
2019-11-07 17:08:38 +1300

    got wysiwyg working again

--- routes/reset-routes.js
+++ routes/reset-routes.js
@@ -3,71 +3,71 @@ const router = express.Router();
 const userDao = require("../modules/users-dao.js");
 
 // The setup for the mailer functionality
-const nodemailer = require('nodemailer');  
+const nodemailer = require('nodemailer');
 
 
-router.get("/forgotpsw", async function(req, res) { 
-       
+router.get("/forgotpsw", async function (req, res) {
+
     //const context = {message: "Please enter your Email Address below"}
     res.locals.message = req.query.message;
-    
-    
-    res.render("forgotpsw"); 
-}); 
 
 
-router.post("/forgotpsw", async function(req, res) { 
-    const email = req.body.email_address;  
-    
-     
-    try { 
-        const sendEmail = await userDao.retrieverUserEmail(email);  
-        
-        if (sendEmail) { 
-          
-        
+    res.render("forgotpsw");
+});
+
+
+router.post("/forgotpsw", async function (req, res) {
+    const email = req.body.email_address;
+
+
+    try {
+        const sendEmail = await userDao.retrieverUserEmail(email);
+
+        if (sendEmail) {
+
+
             let transporter = nodemailer.createTransport({
-                service: 'gmail', 
-                auth: { 
-                    user: 'gshkblogsecure@gmail.com', 
+                service: 'gmail',
+                auth: {
+                    user: 'gshkblogsecure@gmail.com',
                     pass: 'gshkm4il3r'
-                },                   
-            });  
-        
+                },
+            });
+
             let mailOptions = {
-                from: 'gshkblogsecure@gmail.com', 
-                to: `${email}`, 
-                subject: 'Reset your password!',  
+                from: 'gshkblogsecure@gmail.com',
+                to: `${email}`,
+                subject: 'Reset your password!',
                 // create username variable
                 html: `Hi <strong>${sendEmail.username}</strong>,<br><br> 
                 We recieved a request to reset your password.<br> 
                 Please click the link below to <br><br> 
                 <p><a href='http://localhost:3000/resetpsw'>Reset your password!</a></p>`
-            };  
+            };
 
-            transporter.sendMail(mailOptions, function(error, info) {
+            transporter.sendMail(mailOptions, function (error, info) {
                 if (error) {
-                  console.log(error);
+                    console.log(error);
                 } else {
-                  console.log('Email sent: ' + info.response);
+                    console.log('Email sent: ' + info.response);
                 }
-              });  
-              
-            req.session.username = sendEmail.username; 
-             req.session.email = email; 
-             console.log(req.session.email);  
-             res.redirect("/forgotpsw?message=A message has been sent to you by email with instructions on how to reset your password. (Please check your Junk Box too!)");    
-//&#9989; 
-        } else { 
-            
-            res.redirect("/forgotpsw"); 
-            
+            });
+
+            req.session.username = sendEmail.username;
+            req.session.email = email;
+            console.log(req.session.email);
+            res.redirect("/forgotpsw?message=A message has been sent to you by email with instructions on how to reset your password. (Please check your Junk Box too!)");
+            //&#9989; 
+        } else {
+
+            res.redirect("/forgotpsw");
+
         }
     } catch (err) {
         console.log(err);
     };
-   
-}); 
+
+});
 
 
 router.get("/resetpsw", function (req, res) {
